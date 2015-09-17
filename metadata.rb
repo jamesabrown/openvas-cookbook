63e7cdb07aa6a10b3f5c517e2ee228bd1e69dcec
Christian Parkinson <cparkinson@invoca.com>
2015-09-17 13:42:15 -0700

    Adds a Vagrantfile ERB

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,8 @@
-require 'revops/versioning'
-
 name             'invocaops_app_skeleton'
 maintainer       'Invoca Operations'
 maintainer_email 'operations@invoca.com'
 license          'All rights reserved'
 description      'Installs/Configures invocaops_app_skeleton'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          Revops::Versioning.generate_version
+version          '0.1.0'
+
