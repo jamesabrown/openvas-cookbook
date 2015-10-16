b8a15b51ea57a50340361e627a91bc5ecf963c7f
James Brown <jbrown@invoca.com>
2015-10-16 13:08:32 -0700

    initial commit

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,8 @@
-name             'invocaops_app_skeleton'
-maintainer       'Invoca Operations'
-maintainer_email 'operations@invoca.com'
+name             'open-vas-cookbook'
+maintainer       'James Brown'
+maintainer_email 'jbrown@thejbproject.com'
 license          'All rights reserved'
 description      'Installs/Configures invocaops_app_skeleton'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '1.0.0'
 
